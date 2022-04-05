846b5a2d0a7f29c79b407f63dca922e4874daf9e
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-04-05 09:31:40 -0700

    Re-enable sound! Argh!

--- src/global/game_global.gd
+++ src/global/game_global.gd
@@ -76,9 +76,9 @@ func _get_manifest_overrides() -> Array:
             Sc.modes.get_is_active("annotations", "emphasized")
     
     var moves_debug_game_window_to_other_monitor := false
-#    var debug_window_size = ScaffolderGuiConfig.SCREEN_RESOLUTIONS.default
+    var debug_window_size = ScaffolderGuiConfig.SCREEN_RESOLUTIONS.default
 #    var debug_window_size = ScaffolderGuiConfig.SCREEN_RESOLUTIONS.full_screen
-    var debug_window_size = ScaffolderGuiConfig.SCREEN_RESOLUTIONS.youtube
+#    var debug_window_size = ScaffolderGuiConfig.SCREEN_RESOLUTIONS.youtube
     
     if recording:
 #        debug_window_size = ScaffolderGuiConfig.SCREEN_RESOLUTIONS.youtube
